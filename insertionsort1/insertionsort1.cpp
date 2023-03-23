bcf1609cec23055dc9903c542bec7be37b187161
IAMHUDA <hudamiftahul362@gmail.com>
2023-03-23 12:37:43 +0700

    membuat prosedur input jumlah elemen array dan memanggil variabel inputan nilai n dan membuat tampilan susunan data padaa elemen array

--- insertionsort1/insertionsort1.cpp
+++ insertionsort1/insertionsort1.cpp
@@ -3,3 +3,29 @@ using namespace std;
 
 int arr[20];        //membuat array dengan panjang data 
 int n;              //membuat variabel inputan n .
+
+void input() {            //prosedur input
+	while (true)
+	{
+		cout << "masukan jumlah data pada array :";   //membuat inputan jumlah element array
+		cin >> n;                         //memanggil variabel inputan n
+
+		if (n <= 20) {                   //membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukan makksimal 20 elemen , \n"; //menampilkan pesan jika data lebih dari 20
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;   //membuat tampilan susunan data pada elemen array
+	cout << "masukan elemen array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-" << (i + 1) << " :";
+		cin >> arr[i];
+	}
+}
